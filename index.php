b02c05a14298feef1212bfe7906888cd699c7d8f
Ilya Birman <ilyabirman@ilyabirman.ru>
2011-11-14 03:54:28 +0600

    Add comments

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?
 
+/*
+профили конфига
+вариант для комментариев, без хтмла
+дефис после неразрывного пробела не заменится на тире
+
+*/
+
 header ('Content-Type: text/html; charset=utf-8');
 error_reporting (E_ALL & ~E_NOTICE);
 
