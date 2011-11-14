ebbf75985ba8f429c1229207ce0a9b12c613e5fb
Ilya Birman <ilyabirman@ilyabirman.ru>
2011-11-14 12:53:59 +0600

    Refactor scaleimage

--- js/scaleimage.js
+++ js/scaleimage.js
@@ -1,51 +1,55 @@
-function imageShowRealSize () {
-
-  this.blur ()
-  $ (this).addClass ('img-zoomed-in')
-  
-  var $img = $ ('img', $ (this))
-  
-  $img.data ({ 'previewWidth': $img.width () })
-
-  fullWidth = $ (this).attr ('width')
-
-  // full picture src is a’s href
-  fullSrc = this.href
-  
-  bigImg = new Image ()
-  $ (bigImg).attr ('src', fullSrc);
-  $ (bigImg).bind ('load', function () {
-    $img.attr ('src', fullSrc)
-  })
-  
-  $img.stop ()
-  $img.css ('height', 'auto')
-  $img.animate ({ width: fullWidth }, 'fast')
-  
-  return false
-  
-}
-
-function imageShowPreviewSize () {
-
-  this.blur ()
-  $ (this).removeClass ('img-zoomed-in')
-  
-  var $img = $ ('img', $ (this))
-  
-  previewWidth = $img.data ('previewWidth')
-  
-  $img.stop ()
-  $img.css ('height', 'auto')
-  $img.animate ({ width: previewWidth }, 'fast')
-  
-  return false
-  
-}
-
-
 if ($) $ (function () {
+  
+  var imageAnimateWidthTo = function ($img, width) {
+    
+    $img.stop ()
+    $img.css ('height', 'auto')
+    $img.animate ({ width: width }, 'fast')
+    
+    return false
+    
+  }
+
+  
+  var imageShowRealSize = function () {
+  
+    this.blur ()
+    $ (this).addClass ('img-zoomed-in')
+    
+    var $img = $ ('img', $ (this))
+    
+    $img.data ({ 'previewWidth': $img.width () })
+  
+    fullWidth = $ (this).attr ('width')
+  
+    // full picture src is a’s href
+    fullSrc = this.href
+    
+    bigImg = new Image ()
+    $ (bigImg).attr ('src', fullSrc);
+    $ (bigImg).bind ('load', function () {
+      $img.attr ('src', fullSrc)
+    })
+    
+    return imageAnimateWidthTo ($img, fullWidth)
+    
+  }
+  
+  var imageShowPreviewSize = function () {
+  
+    this.blur ()
+    $ (this).removeClass ('img-zoomed-in')
+    
+    var $img = $ ('img', $ (this))
+    
+    previewWidth = $img.data ('previewWidth')
+    
+    return imageAnimateWidthTo ($img, previewWidth)
+    
+  }
+  
   $ ('a.link-to-big-picture')
     .toggle (imageShowRealSize, imageShowPreviewSize)
     .append('<div class="zoom-helper"></div>');
+
 })
