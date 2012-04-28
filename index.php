51145a8531373173e50af7ff25f74961b6be534e
Ilya Birman <ilyabirman@ilyabirman.ru>
2012-04-29 02:35:27 +0600

    Content update

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ error_reporting (E_ALL);
 
 if (!include 'neasden/neasden.php') die ('neasden init failed');
 
-$text = file_get_contents ('tests/test-9.txt');
+$text = file_get_contents ('tests/test-11.txt');
 $res = '';
 
 function stopwatch () {
